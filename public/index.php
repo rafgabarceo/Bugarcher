3df1f005837fa40a3cc8bee10bad5496a9421afc
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-09-08 15:09:29 +0800

    Added index.php for the landing page. Currently just prints out Document Root

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>LBYCPG2 Bug Tracker</title>
+        <?php require_once("../resources/config.php"); ?>
+        <?php require_once("../resources/templates/header.php");?>
+    </head>
+    <body>
+        <?php 
+            $var = $_SERVER["DOCUMENT_ROOT"];
+            echo $var;
+        ?> 
+    </body>
+</html>
