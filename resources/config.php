1e35df57e186c05359613aeea2b086115f2d8500
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-09-08 15:14:21 +0800

    Added basic inclution for bootstrap

---
+++ resources/config.php
@@ -0,0 +1,6 @@
+<?php 
+
+    echo '<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <script type="text/JavaScript" src="assets/bootstrap/js/bootstrap.min.js"></script>';
+
+ ?>
