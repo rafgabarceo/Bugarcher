159a9b8610d0013c10322cf4caa55551de2593b2
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-09-11 16:43:12 +0800

    Added fields for inputting a new bug. Also added button for new project

--- public/home.php
+++ public/home.php
@@ -16,66 +16,113 @@
     } 
 
     // require_once("templates/loggedheader.php");
+    if($_SERVER["REQUEST_METHOD" == "POST"]){
 
-    $conn = new mysqli("127.0.0.1", "root", "", "bugtracker");
-
-    if(!$conn){
-        echo "Error connecting to database. Error: ".$conn->error;
-        die();
-    }
-
-    // get all the user information, including the bugs that they have issued. print_r to verfy information.
-    // gets the information about the user. 
-    $stmt = $conn->prepare("SELECT `username`, `role` FROM user WHERE `email`=?");
-    if($stmt){
-        $stmt->bind_param("s", $_SESSION["email"]);
-        $stmt->execute();
     } else {
-        print_r($conn->error);
-        var_dump($stmt);
+        $conn = new mysqli("127.0.0.1", "root", "", "bugtracker");
+
+        if(!$conn){
+            echo "Error connecting to database. Error: ".$conn->error;
+            die();
+        }
+
+        // get all the user information, including the bugs that they have issued. print_r to verfy information.
+        // gets the information about the user. 
+        $stmt = $conn->prepare("SELECT `username`, `role` FROM user WHERE `email`=?");
+        if($stmt){
+            $stmt->bind_param("s", $_SESSION["email"]);
+            $stmt->execute();
+        } else {
+            print_r($conn->error);
+            var_dump($stmt);
+        }
+
+        $userDetails = $stmt->get_result()->fetch_array();
+        
+        unset($stmt);
+
+        $stmt = $conn->prepare("SELECT `project_id`, `description`, `project_id` FROM project WHERE `user_id` = (SELECT `user_id` FROM user WHERE `email`=?)");
+        if($stmt){
+            $stmt->bind_param("s", $_SESSION["email"]);
+            $stmt->execute();
+        } else {
+            var_dump($stmt);
+        }
+
+        $projectDetails = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        unset($stmt);
+
+        $stmt = $conn->prepare("SELECT `bug_id`, `created_at`, `project_id`, `status`, `title` FROM bug WHERE `user_id` = (SELECT `user_id` FROM user WHERE `email`=?)");
+        if($stmt){
+            $stmt->bind_param("s", $_SESSION["email"]);
+            $stmt->execute();
+        } else {
+            print_r($conn->error);
+        }
+        
+        $bugDetails = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        unset($stmt);
+
+        $conn->close();
     }
 
-    $userDetails = $stmt->get_result()->fetch_array();
-    
-    unset($stmt);
-
-    $stmt = $conn->prepare("SELECT `project_id`, `description`, `project_id` FROM project WHERE `user_id` = (SELECT `user_id` FROM user WHERE `email`=?)");
-    if($stmt){
-        $stmt->bind_param("s", $_SESSION["email"]);
-        $stmt->execute();
-    } else {
-        var_dump($stmt);
-    }
-
-    $projectDetails = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-
-    unset($stmt);
-
-    $stmt = $conn->prepare("SELECT `bug_id`, `created_at`, `project_id`, `status`, `title` FROM bug WHERE `user_id` = (SELECT `user_id` FROM user WHERE `email`=?)");
-    if($stmt){
-        $stmt->bind_param("s", $_SESSION["email"]);
-        $stmt->execute();
-    } else {
-        print_r($conn->error);
-    }
-    
-    $bugDetails = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-
-    unset($stmt);
-
-    $conn->close();
 ?>
 <body>
     <div class="container-fluid mt-5">
         <div class="row">
             <div class="col">
                 <div class="card">
-                    <div class="card-header">
+                    <div class="card-header d-flex justify-content-between align-items-center">
                         <?php echo '<h1>'.$userDetails['username'].'</h1>';?>
+                        <button type="button" class="btn btn-sm btn-primary">New Project</button>
                     </div>
                     <div class="card-content p-3">
-                        This is where various options such as add new bug, etc etc should be.
-                    </div>
+                        <div class="row">
+                            <div class="col">
+                                <h3>Report bug</h3>
+                            </div>
+                            <div class="col">
+                                <h3>Project</h3>
+                            </div>
+                        </div>
+                        <form>
+                        <div class="row">
+                                <div class="col">
+                                    <input type="text" action="" class="form-control form-control-lg" placeholder="New Bug Title"/>
+                                </div>
+                                <div class="col">
+                                    <?php 
+                                    
+                                        echo "<select class='form-control'>"; 
+
+                                        foreach($projectDetails as $project){
+                                            echo "<option>".$project["title"]."</option>";
+                                        }
+
+                                        echo "</select>";
+                                        
+                                    ?>
+                                </div>
+                            <div class="row p-3">
+                                <div class="col">
+                                    <h3>Hardware information</h3>
+                                    <textarea type="text" class="form-control" rows=5 placeholder="Hardware information. Include operating system, etc. etc." name="bug_hwinfo"></textarea> 
+                                </div>
+                            </div>
+                            <div class="row p-3">
+                                <div class="col">
+                                    <h3>
+                                        Information
+                                    </h3>
+                                    <textarea type="text" class="form-control" rows=10 placeholder="Bug information"></textarea> 
+                                </div>
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="File bug"/>
+                            </form> 
+                        </div>
+                   </div>
                 </div>
             </div>
             <div class="col">
