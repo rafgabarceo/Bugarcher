0bca22e30406e95ef21f856f012f44039a0417d2
Rafael Gabriel Arceo <arceo.rafaegabriel@protonmail.com>
2021-09-12 17:30:59 +0800

    Added additional configuration for new tooltip

--- resources/config.php
+++ resources/config.php
@@ -1,6 +1,7 @@
 <?php 
 
     echo '<link rel="stylesheet" href="../public/assets/bootstrap/css/bootstrap.min.css">
-    <script type="text/JavaScript" src="../public/assets/bootstrap/js/bootstrap.min.js"></script>';
+    <script type="text/JavaScript" src="../public/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/JavaScript" src="../public/tooltipActive.js"></script>';
 
  ?>
